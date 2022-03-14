cc56138eccd054ddb793c6cbc2fc0419ad2f3af6
Aditya Raj <ar8342077@gmail.com>
2022-03-13 23:14:47 -0400

    alll react data so far

--- my-app/src/component/TextForm.js
+++ my-app/src/component/TextForm.js
@@ -7,9 +7,16 @@ export default function TextForm(props) {
     let newtxt=txt.toUpperCase();
     setText(newtxt);
   }
+  const dp=()=>{
+    let a=txt.toLowerCase();
+    setText(a);
+  }
   const hc= (event)=>{
     console.log("get the input ");
     setText(event.target.value);
+  }
+  const sp=()=>{
+    setText("");
   }
   const [txt, setText] = useState('ENTER text Here');
   return (
@@ -19,9 +26,18 @@ export default function TextForm(props) {
         <div className="mb-3">
         
         <textarea className="form-control" id="myinput" rows="8" value={txt} onChange={hc}></textarea>
-        <button className='btn btn-primary' onClick={upclick}>Convert to UpparCase</button>
-
+        <button className='btn btn-primary mx-1' onClick={upclick}>Convert to UpparCase</button>
+        <button className='btn btn-danger mx-1' onClick={dp}>CONVERT TO LOWERCASE</button>
+        <button className='btn btn-success mx-1' onClick={sp}>CLEAR</button>
     </div>
+    <div className='container'>
+        <h1> THIS IS YOUR TEXT SUMMARY</h1>
+        <p> Number of Words: {txt.length}  </p>
+        <p>Number of Letters:{txt.split(" ").length}</p>
+        <p>{0.008 * txt.split(" ").length} Minutes To Read</p>
+        <h2>PREVIEW</h2>
+        {txt}
+      </div>
     </>
   )
 }
